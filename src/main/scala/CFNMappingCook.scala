2b94eb4daa0ae17de673d75360ada774242b827d
Francesco Perera <fperera29@gmail.com>
2017-03-22 12:04:19 -0400

    updated column field map

--- src/main/scala/CFNMappingCook.scala
+++ src/main/scala/CFNMappingCook.scala
@@ -5,22 +5,27 @@ object CFNMappingCook {
     * */
 
   val cfnMap:Map[String,Array[String]] = Map ( //TODO: Fucking manual work. Automate this ASAP
-    "email_address" -> Array("email_address","email","emailaddress"),
+    "email_address" -> Array("email_address","email","emailaddress","emails"),
     "full_name" -> Array("full_name","fullname","name","employee"),
     "first_name" -> Array("first_name","firstname"),
     "last_name" -> Array("last_name","lastname","surname","family_name"),
     "middle_name" -> Array("middle_name","middlename","middle"),
     "name_modifier" -> Array("modifier"),
-    "address" -> Array("address","physical_address","physicaladdress","employer_address","work_address"),
+    "address" -> Array("address","physical_address","physicaladdress","employer_address","work_address","waddress","street_address"),
     "phone_number" -> Array("phone_number","phonenumber","phone"),
     "username" -> Array("username","user_name"),
     "education"-> Array("college","university","school"),
     "employee_id" -> Array("employee_id"),
     "uuid" -> Array("uuid"),
-    "job_title" -> Array("job_title","jobtitle","occupation","occupation_title","jobTitle"), //TODO: double check  difference between occupation and job title
+    "job" -> Array("job","occupation"),
+    "job_title" -> Array("job_title","jobtitle","occupation_title","jobTitle"),
     "salary" -> Array("salary"),
     "employer" -> Array("employer"),
     "company_name" -> Array("companyName"),
+    "credit_card_number" -> Array("credit_card_number"),
+    "date" -> Array("date"),
+    "ip_number" -> Array("ip_number"),
+    "vehicle_plate" -> Array("vehicle_plate"),
     "all" -> Array()
   )
 
